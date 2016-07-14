fe7b908459a4a8cfd0dc74063098329efa523984
Ryan McCue <me@ryanmccue.info>
2016-07-14 10:53:39 +0100

    Refactor common ReactDOM call

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+let render = app => {
+	ReactDOM.render(<AppContainer>{app}</AppContainer>, document.getElementById( 'app' ))
+}
+
 if (module.hot) {
 	module.hot.accept('./components/App', () => {
 		// If you use Webpack 2 in ES modules mode, you can
 		// use <App /> here rather than require() a <NextApp />.
-		const NextApp = require('./components/App').default;
-		ReactDOM.render(
-			<AppContainer>
-				<NextApp />
-			</AppContainer>,
-			document.getElementById( 'app' )
-		);
+		const NextApp = require('./components/App').default
+		render(<NextApp />)
 	})
 }
 
-ReactDOM.render( <AppContainer><App /></AppContainer>, document.getElementById( 'app' ) )
+render(<App />)
