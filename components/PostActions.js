c6a38f1756a53e081fb87c90ebe93c7788ceef86
Ryan McCue <me@ryanmccue.info>
2016-07-14 14:29:32 +0100

    Add draft actions

--- components/PostActions.js
+++ components/PostActions.js
@@ -9,12 +9,28 @@ export default class PostActions extends React.Component {
 	}
 
 	render() {
-		let liked = false
-		return <div className="actions">
-			<button
-				className="action-like"
-				onClick={() => this.setState({liked: !this.state.liked})}
-			>{ this.state.liked ? "♥" : "♡" }</button>
-		</div>
+		let { post } = this.props
+		let published = post.status === "published"
+
+		// fake it 'til we make it
+		published = post.id % 2
+
+		if (published) {
+			return <div className="actions">
+				<button
+					className="action-like"
+					onClick={() => this.setState({liked: !this.state.liked})}
+				>{ this.state.liked ? "♥" : "♡" }</button>
+			</div>
+		} else {
+			return <div className="actions">
+				<button
+					className="secondary"
+				>Reject</button>
+				<button
+					className="primary"
+				>Approve</button>
+			</div>
+		}
 	}
 }
