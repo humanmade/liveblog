ab8c4e0c883b1f549a4dda1faf30f417d825a64c
Ryan McCue <me@ryanmccue.info>
2016-07-14 10:55:01 +0100

    Reformat build tools

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var webpack = require('webpack')
+var WebpackDevServer = require('webpack-dev-server')
+var config = require('./webpack.config')
 
 new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
+	publicPath: config.output.publicPath,
+	hot: true,
+	historyApiFallback: true
 }).listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
+	if (err) {
+		return console.log(err);
+	}
 
-  console.log('Listening at http://localhost:3000/');
-});
+	console.log('Listening at http://localhost:3000/');
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      exclude: /node_modules/,
-      query: {
-        presets: ['es2015', 'react', 'stage-1']
-      }
-    }]
-  }
-};
+	devtool: 'eval',
+	entry: [
+		'react-hot-loader/patch',
+		'webpack-dev-server/client?http://localhost:3000',
+		'webpack/hot/only-dev-server',
+		'./index'
+	],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	],
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			loader: 'babel',
+			exclude: /node_modules/,
+			query: {
+				presets: ['es2015', 'react', 'stage-1']
+			}
+		}]
+	}
+}
