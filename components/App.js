e8b8bb1f610970629802972e143f7e4df5edc5d1
Ryan McCue <me@ryanmccue.info>
2016-07-15 15:19:15 +0100

    Authenticate using the API

--- components/App.js
+++ components/App.js
@@ -4,6 +4,9 @@ import Header from './Header'
 import PostsList from './PostsList'
 import Welcome from './Welcome'
 
+const API_KEY = 'wMa5iV8tJII5'
+const API_SECRET = 'a8xu7QZVBRZStXaWz6ewcL9C2NmUOX8F1C0V99YUD2FkoaS5'
+
 export default class App extends React.Component {
 	constructor() {
 		super()
@@ -11,21 +14,42 @@ export default class App extends React.Component {
 			posts: [],
 			url: '',
 			site: null,
+			user: null,
+		}
+	}
+
+	componentWillMount() {
+		let url = window.localStorage.getItem( 'url' )
+		if ( url ) {
+			this.onConnect(url)
 		}
 	}
 
 	onConnect(url) {
 		this.setState({ url: url })
+		window.localStorage.setItem( 'url', url )
 
-		this.api = new api({
-			url: url
+		let apiHandler = window.apiHandler = new api({
+			url: url,
+			brokerURL: 'https://apps.wp-api.org/',
+			brokerCredentials: {
+				client: {
+					public: API_KEY,
+					secret: API_SECRET,
+				},
+			},
+			callbackURL: window.location,
 		})
 
-		this.api.get( '/' )
-			.then( site => this.setState( { site: site } ) )
+		apiHandler.restoreCredentials().authorize().then( () => {
+			apiHandler.saveCredentials()
 
-		this.api.get( '/wp/v2/posts?_embed' )
-			.then( posts => this.setState( { posts: posts } ) )
+			apiHandler.get( '/' )
+				.then(site => this.setState({ site: site }))
+
+			apiHandler.get( '/wp/v2/posts', { _embed: true } )
+				.then(posts => this.setState({ posts: posts }))
+		})
 	}
 
 	render() {
