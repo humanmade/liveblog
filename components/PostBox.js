14d871cf86dd573c0a5b429bae276326dc098f2e
Ryan McCue <me@ryanmccue.info>
2016-07-15 15:23:14 +0100

    Store postbox text in state

--- components/PostBox.js
+++ components/PostBox.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
 export default class PostBox extends React.Component {
+	constructor() {
+		super()
+
+		this.state = {
+			text: '',
+		}
+	}
+
 	render() {
 		return <div className="post-box">
-			<textarea rows={4} />
+			<textarea rows={4} onChange={e => this.setState({ text: e.target.value })} />
 			<p className="actions">
-				<button className="secondary">Submit for Review</button>
-				<button className="primary">Publish</button>
+				<button className="secondary" onClick={() => this.props.onSubmit(this.state.text)}>Submit for Review</button>
+				<button className="primary" onClick={() => this.props.onPublish(this.state.text)}>Publish</button>
 			</p>
 		</div>
 	}
+}
+
+PostBox.propTypes = {
+	onSubmit: React.PropTypes.func.isRequired,
+	onPublish: React.PropTypes.func.isRequired,
 }
