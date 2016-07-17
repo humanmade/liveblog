1ba3ba12e79ed7129cb23e133657a4663e9c6edb
Joe Hoyle <joehoyle@gmail.com>
2016-07-17 19:42:20 +0100

    Add fetch() polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var webpack = require('webpack')
 module.exports = {
 	devtool: 'eval',
 	entry: [
+		'whatwg-fetch',
 		'react-hot-loader/patch',
 		'webpack-dev-server/client?http://localhost:3000',
 		'webpack/hot/only-dev-server',
