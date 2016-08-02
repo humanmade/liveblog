9759ce12cf0029c4c725338b4bf1ab20d152e95b
Joe Hoyle <joehoyle@gmail.com>
2016-08-02 11:41:19 +0200

    Add dummy data

--- components/App.js
+++ components/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import Header from './Header'
 import PostsList from './PostsList'
+import Data from '../data'
 
 export default class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			posts: Data
+		}
+	}
 	render() {
 		return <div className="app">
 			<Header />
 			<div className="posts">
-				<PostsList posts={[]} />
+				<PostsList posts={this.state.posts} />
 			</div>
 		</div>
 	}
