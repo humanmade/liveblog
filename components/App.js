8fd120bb8c126d6081a22c38fca18566290f266e
Joe Hoyle <joehoyle@gmail.com>
2016-08-02 11:34:36 +0200

    Add base components

--- components/App.js
+++ components/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Header from './Header'
+import PostsList from './PostsList'
 
 export default class App extends React.Component {
 	render() {
-		return <div>Hello World!</div>
+		return <div className="app">
+			<Header />
+			<div className="posts">
+				<PostsList posts={[]} />
+			</div>
+		</div>
 	}
 }
---
+++ components/PostsList.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Post from './Post'
+
+export default class PostsList extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			filter: 'all',
+		}
+	}
+
+	render() {
+		let posts = this.props.posts
+
+		if (this.state.filter !== 'all') {
+			posts = posts.filter( post => post.status === this.state.filter )
+		}
+
+		return <div className="PostsList">
+			<p className="post-filter">
+				{this.props.showFilter ?
+					<span className="group">
+						Filter:
+						<a
+							className={ this.state.filter == 'all' ? 'active' : ''}
+							onClick={() => this.setState({ filter: 'all' })}
+						>All</a>
+						<a
+							className={ this.state.filter == 'pending' ? 'active' : ''}
+							onClick={() => this.setState({ filter: 'pending' })}
+						>Pending</a>
+					</span>
+				: null}
+			</p>
+			{posts.map( post =>
+				<Post
+					key={post.id}
+					post={post}
+				/>
+			)}
+		</div>
+	}
+}
