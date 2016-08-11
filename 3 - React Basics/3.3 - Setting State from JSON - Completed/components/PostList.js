b8d8c5ee1b67dd8c7e83611eb71941f977723f08
Zac Gordon <email@zacgordon.com>
2016-08-11 16:07:52 -0400

    Updating React JSON example

--- 3 - React Basics/3.3 - Setting State from JSON - Completed/components/PostList.js
+++ 3 - React Basics/3.3 - Setting State from JSON - Completed/components/PostList.js
@@ -4,19 +4,11 @@ export default class PostList extends React.Component {
 
   render() {
 
-    let posts = this.props.posts    
+    let posts = this.props.posts
 
     return <div>
 
       {posts.map( post => <p key={post.id}>{post.title}</p> )}
-
-      <button
-        type="Submit"
-        onClick={this.props.refreshPosts}
-      >
-        Refresh Posts
-      </button>
-
 
     </div>
 
