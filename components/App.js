e74a605acb08c0c4c23abf1315c5f27a8e94091b
Joe Hoyle <joehoyle@gmail.com>
2016-08-18 12:07:41 +0200

    Add isLoadingPosts to PostsList

--- components/App.js
+++ components/App.js
@@ -19,6 +19,8 @@ export default class App extends React.Component {
 		this.loadPosts()
 	}
 	loadPosts() {
+		this.setState({ isLoadingPosts: true })
+
 		let args = {
 			_embed: true,
 			per_page: 100,
@@ -26,14 +28,14 @@ export default class App extends React.Component {
 
 		apiHandler.get('/wp/v2/posts', args)
 			.then(posts => {
-				this.setState({ posts })
+				this.setState({ posts, isLoadingPosts: false })
 			})
 	}
 	render() {
 		return <div className="app">
 			<Header />
 			<div className="posts">
-				<PostsList posts={this.state.posts} />
+				<PostsList posts={this.state.posts} isLoadingPosts={this.state.isLoadingPosts} />
 			</div>
 		</div>
 	}
