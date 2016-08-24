8d4c2101f1eafdea3fbdffcf0e326ab366ec7e27
Joe Hoyle <joehoyle@gmail.com>
2016-08-24 15:13:59 +0200

    Update site url

--- components/App.js
+++ components/App.js
@@ -3,7 +3,7 @@ import api from 'wordpress-rest-api-oauth-1'
 import Header from './Header'
 import PostsList from './PostsList'
 
-const SITE_URL = 'http://wordpress.dev/'
+const SITE_URL = 'https://aweekofrest.hm/liveblog/'
 
 export default class App extends React.Component {
 	constructor() {
