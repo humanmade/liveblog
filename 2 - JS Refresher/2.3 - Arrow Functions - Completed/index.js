48f811e638d5a5fdd0a2f85dd2747de4f76b09df
Zac Gordon <email@zacgordon.com>
2016-09-03 09:43:24 -0400

    Cleaning up exercise files and updating Readme

--- 2 - JS Refresher/2.3 - Arrow Functions - Completed/index.js
+++ 2 - JS Refresher/2.3 - Arrow Functions - Completed/index.js
@@ -16,12 +16,8 @@ let posts = [
     drafts
 
 
-// Rewrite map with an arrow function
 postList = posts.map( post => '<li>' + post.title + '</li>' )
-
 console.log( postList )
 
-// Rewrite filter with an arrow function
 drafts = posts.filter( post => post.status === 'draft' )
-
 console.log( drafts )
--- 2 - JS Refresher/2.3 - Arrow Functions/README.md
+++ 2 - JS Refresher/2.3 - Arrow Functions/README.md
@@ -1,5 +1,8 @@
 ### Instructions
 
+- Run 'npm install'
 - Open index.js
 - Rewrite map with an arrow function
 - Rewrite filter with an arrow function
+- Run 'webpack --watch'
+- Test that proper costs are logged in the browser
